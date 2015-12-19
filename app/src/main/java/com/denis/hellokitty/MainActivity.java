64812c8423bf3f6192e93c9afdbfb4dd90cf6417
denis <deoniisii@gmail.ru>
2015-12-19 14:31:00 +0200

    Initial commit

---
+++ app/src/main/java/com/denis/hellokitty/MainActivity.java
@@ -0,0 +1,58 @@
+package com.denis.hellokitty;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelloTextView = (TextView) findViewById(R.id.SetTextTextView);
+        mNameEditText = (EditText) findViewById(R.id.NameEditText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onCliK(View view) {
+
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        }else mHelloTextView.setText("Hello " + mNameEditText.getText());
+    }
+}
